7c70db0bea459246bf9afab6d7ff8d25516baf87
ssomagani <ssomagani@voltdb.com>
2021-09-29 21:46:20 -0400

    Added partition leadership stats

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,9 +15,9 @@ public class Application {
 
 	public static void main(String[] args) throws Exception {
 		
-		String hostStr = args[0];
-		String partColType = args[1];
-		String partColVal = args[2];
+	    String hostStr = "localhost";
+	    if(args.length != 0)
+	        hostStr = args[0];
 		
 		config = new ClientConfig();
 		config.setTopologyChangeAware(true);
@@ -29,19 +29,37 @@ public class Application {
 		Object[] statsArgs = {"PROCEDURE", 0};
 		client.callProcedure("@Statistics", statsArgs);
 		
-		Object[] partArgs = {"PLANNER", 0};
-		ClientResponse resp = client.callProcedure("@Statistics", partArgs);
-		VoltTable partSiteMapTable = resp.getResults()[0];
-		HashMap<Long, String> partSiteMap = new HashMap<>();
-		while(partSiteMapTable.advanceRow()) {
-			partSiteMap.put(partSiteMapTable.getLong(4), partSiteMapTable.getLong(3) + "@" + partSiteMapTable.getString(2));
-		}
-		
-		if(!client.isHashinatorInitialized())
-			throw new Exception("Hashinator not initialized. Cannot Proceed.");
-		
-		long partId = client.getPartitionForParameter(VoltType.typeFromString(partColType).getValue(), partColVal);
-		System.out.println("Partition column value " + partColVal + " --> PartitionId:SiteId@Hostname = " + partId + ":" + partSiteMap.get(partId));
+        Object[] partArgs = {"PLANNER", 0};
+        ClientResponse resp = client.callProcedure("@Statistics", partArgs);
+        VoltTable partSiteMapTable = resp.getResults()[0];
+        HashMap<Long, String> partSiteMap = new HashMap<>();
+        HashMap<Long, String> partHostMap = new HashMap<>();
+        while(partSiteMapTable.advanceRow()) {
+            partSiteMap.put(partSiteMapTable.getLong(4), partSiteMapTable.getLong(3) + "@" + partSiteMapTable.getString(2));
+            partHostMap.put(partSiteMapTable.getLong(4), partSiteMapTable.getString(2));
+        }
+        
+		Object[] topoArgs = {"TOPO", 0};
+		ClientResponse topo = client.callProcedure("@Statistics", topoArgs);
+        VoltTable topoTable = topo.getResults()[0];
+        System.out.println("----------------------------------------------");
+        System.out.println("| Host              Partition  Sites  Leader |");
+        System.out.println("------------------ ---------- ------ ---------");
+        while(topoTable.advanceRow()) {
+            System.out.println(String.format("%1s %16s %10s %6s %7s %1s", "|", partHostMap.get(topoTable.getLong(0)), topoTable.getLong(0), topoTable.getString(1), topoTable.getString(2), "|"));
+        }
+        System.out.println("----------------------------------------------");
+        
+//		if(!client.isHashinatorInitialized())
+//			throw new Exception("Hashinator not initialized. Cannot Proceed.");
+//		
+//        String partColType = args[1];
+//        String partColVal = args[2];
+//
+//        long partId = client.getPartitionForParameter(VoltType.typeFromString(partColType).getValue(), partColVal);
+//        
+//		System.out.println("Partition column value " + partColVal + " --> PartitionId:SiteId@Hostname = " 
+//		+ partId + ":" + partSiteMap.get(partId));
 	}
 	
 	private static void connect(String servers) throws InterruptedException {
